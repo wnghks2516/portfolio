b6da924bc101fd64ab47ee43c127ee5f61b929a7
wnghks2516 <wnghks2516@naver.com>
2024-04-16 18:28:41 +0900

    feat : 프로젝트 생성

---
+++ Source/Portfolio/PortfolioGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PortfolioGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_API APortfolioGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
