39001bb33ca3891db8d6c85b1ef99b1e2a21037f
wnghks2516 <wnghks2516@naver.com>
2024-04-23 17:54:14 +0900

    ObjectClass 추가.

---
+++ Source/Portfolio/Public/SUnrealObjectClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "SUnrealObjectClass.generated.h"
+/**
+ * 
+ */
+UCLASS()
+class PORTFOLIO_API USUnrealObjectClass : public UObject
+{
+	GENERATED_BODY()
+};
