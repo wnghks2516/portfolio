69be34e9c40b3f9e16bf892a574cb4df8023f311
wnghks2516 <wnghks2516@naver.com>
2024-05-13 17:29:16 +0900

    오류 수정 #include "UObject/SavePackage.h" 추가

--- Source/Portfolio/Private/Game/SGameInstance.cpp
+++ Source/Portfolio/Private/Game/SGameInstance.cpp
@@ -7,6 +7,7 @@
 #include "Example/SFlyable.h"
 #include "Example/Spigeon.h"
 #include "JsonObjectConverter.h"
+#include "UObject/SavePackage.h"
 
 USGameInstance::USGameInstance()
 {
@@ -34,33 +35,91 @@ void USGameInstance::Init()
 	FPaths::MakeStandardFilename(AbsolutePathForRawData);
 	UE_LOG(LogTemp, Log, TEXT("Absolute path for saved file: %s"), *AbsolutePathForRawData);
 
-    const FString JsonDataFileName(TEXT("StudyJsonFile.txt"));
-    FString AbsolutePathForJsonData = FPaths::Combine(*SavedDir, *JsonDataFileName);
-    FPaths::MakeStandardFilename(AbsolutePathForJsonData);
+	FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*AbsolutePathForRawData);
+	if (nullptr != RawFileWriterAr)
+	{
+		*RawFileWriterAr << SrcRawData;
+		RawFileWriterAr->Close();
+		delete RawFileWriterAr;
+		RawFileWriterAr = nullptr;
+	}
 
-    TSharedRef<FJsonObject> SrcJsonObject = MakeShared<FJsonObject>();
-    FJsonObjectConverter::UStructToJsonObject(SerializedPigeon->GetClass(), SerializedPigeon, SrcJsonObject);
+	FBirdData DstRawData;
+	FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*AbsolutePathForRawData);
+	if (nullptr != RawFileReaderAr)
+	{
+		*RawFileReaderAr << DstRawData;
+		RawFileReaderAr->Close();
+		delete RawFileReaderAr;
+		RawFileReaderAr = nullptr;
 
-    FString JsonOutString;
-    TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);
-    if (true == FJsonSerializer::Serialize(SrcJsonObject, JsonWriterAr))
-    {
-        FFileHelper::SaveStringToFile(JsonOutString, *AbsolutePathForJsonData);
-    }
+		UE_LOG(LogTemp, Log, TEXT("[DstRawData] Name: %s, ID: %d"), *DstRawData.Name, DstRawData.ID);
+	}
 
-    FString JsonInString;
-    FFileHelper::LoadFileToString(JsonInString, *AbsolutePathForJsonData);
-    TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
+	SerializedPigeon = NewObject<USpigeon>();
+	SerializedPigeon->SetName(TEXT("Pigeon76"));
+	SerializedPigeon->SetID(76);
+	UE_LOG(LogTemp, Log, TEXT("[SerializedPigeon] Name: %s, ID: %d"), *SerializedPigeon->GetName(), SerializedPigeon->GetID());
 
-    TSharedPtr<FJsonObject> DstJsonObject;
-    if (true == FJsonSerializer::Deserialize(JsonReaderAr, DstJsonObject))
-    {
-        USpigeon* Pigeon78 = NewObject<USpigeon>();
-        if (true == FJsonObjectConverter::JsonObjectToUStruct(DstJsonObject.ToSharedRef(), Pigeon78->GetClass(), Pigeon78))
-        {
-            UE_LOG(LogTemp, Log, TEXT("[Pigeon78] Name: %s, ID: %d"), *Pigeon78->GetName(), Pigeon78->GetID());
-        }
-    }
+	const FString ObjectDataFileName(TEXT("ObjectData.bin"));
+	FString AbsolutePathForObjectData = FPaths::Combine(*SavedDir, *ObjectDataFileName);
+	FPaths::MakeStandardFilename(AbsolutePathForObjectData);
+
+	TArray<uint8> BufferArray;
+	FMemoryWriter MemoryWriterAr(BufferArray);
+	SerializedPigeon->Serialize(MemoryWriterAr);
+
+	TUniquePtr<FArchive> ObjectDataFileWriterAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileWriter(*AbsolutePathForObjectData));
+	if (nullptr != ObjectDataFileWriterAr)
+	{
+		*ObjectDataFileWriterAr << BufferArray;
+		ObjectDataFileWriterAr->Close();
+
+		ObjectDataFileWriterAr = nullptr; //delete ObjectDataFileWriterAr; 와 같은 효과.
+	}
+
+	TArray<uint8> BufferArrayFromObjectDataFile;
+	TUniquePtr<FArchive> ObjectDataFileReaderAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileReader(*AbsolutePathForObjectData));
+	if (nullptr != ObjectDataFileReaderAr)
+	{
+		*ObjectDataFileReaderAr << BufferArrayFromObjectDataFile;
+		ObjectDataFileReaderAr->Close();
+
+		ObjectDataFileReaderAr = nullptr;
+	}
+
+	FMemoryReader MemoryReaderAr(BufferArrayFromObjectDataFile);
+	USpigeon* Pigeon77 = NewObject<USpigeon>();
+	Pigeon77->Serialize(MemoryReaderAr);
+	UE_LOG(LogTemp, Log, TEXT("[Pigeon77] Name: %s, ID: %d"), *Pigeon77->GetName(), Pigeon77->GetID());
+
+	const FString JsonDataFileName(TEXT("StudyJsonFile.txt"));
+	FString AbsolutePathForJsonData = FPaths::Combine(*SavedDir, *JsonDataFileName);
+	FPaths::MakeStandardFilename(AbsolutePathForJsonData);
+
+	TSharedRef<FJsonObject> SrcJsonObject = MakeShared<FJsonObject>();
+	FJsonObjectConverter::UStructToJsonObject(SerializedPigeon->GetClass(), SerializedPigeon, SrcJsonObject);
+
+	FString JsonOutString;
+	TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);
+	if (true == FJsonSerializer::Serialize(SrcJsonObject, JsonWriterAr))
+	{
+		FFileHelper::SaveStringToFile(JsonOutString, *AbsolutePathForJsonData);
+	}
+
+	FString JsonInString;
+	FFileHelper::LoadFileToString(JsonInString, *AbsolutePathForJsonData);
+	TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
+
+	TSharedPtr<FJsonObject> DstJsonObject;
+	if (true == FJsonSerializer::Deserialize(JsonReaderAr, DstJsonObject))
+	{
+		USpigeon* Pigeon78 = NewObject<USpigeon>();
+		if (true == FJsonObjectConverter::JsonObjectToUStruct(DstJsonObject.ToSharedRef(), Pigeon78->GetClass(), Pigeon78))
+		{
+			UE_LOG(LogTemp, Log, TEXT("[Pigeon78] Name: %s, ID: %d"), *Pigeon78->GetName(), Pigeon78->GetID());
+		}
+	}
 }
 
 void USGameInstance::Shutdown()
