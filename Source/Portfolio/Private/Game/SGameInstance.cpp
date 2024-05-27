1e44a19127d7da4dbc0e962e58ad19af2b0d048b
wnghks2516 <wnghks2516@naver.com>
2024-05-27 15:35:08 +0900

    FString 출력 연습

--- Source/Portfolio/Private/Game/SGameInstance.cpp
+++ Source/Portfolio/Private/Game/SGameInstance.cpp
@@ -20,106 +20,50 @@ void USGameInstance::Init()
 {
 	Super::Init(); // 엔진 업데이트 루틴을 지키기 위해서, 언리얼 엔지니어가 작성한 코드가 먼저 실행되게끔 하기 위함.
 	
-	FBirdData SrcRawData(TEXT("Pigeon17"), 17);
-	UE_LOG(LogTemp, Log, TEXT("[SrcRawData] Name : %s, ID = %d"), *SrcRawData.Name, SrcRawData.ID);
-	//간접경로 
-	const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));
-	UE_LOG(LogTemp, Log, TEXT("SavedDir : %s"), *SavedDir);
 
-	//상대경로
-	const FString RawDataFileName(TEXT("RawData.bin"));
-	FString AbsolutePathForRawData = FPaths::Combine(*SavedDir, *RawDataFileName);
-	UE_LOG(LogTemp, Log, TEXT("Relative path for saved file: %s"), *AbsolutePathForRawData);
+	//UTF-8로 저장해야 한글이 나온다
+	UE_LOG(LogTemp, Log, TEXT("헬로우 언리얼!"));
+	//언리얼 표ㅛ준 문자 자료형이 TCHAR
+	TCHAR ArrayOfChar[] = TEXT("HELLO UNREAL");
+	//문자열을 좀더 자유롭게 조작하고 싶으면 FString을 사용해야 한다.
+	//TCHAR배열을 Wrapping한 헬퍼 클래스가 FString
+	FString String0 = ArrayOfChar;
+	FString String1 = FString(TEXT("HELLO UNREAL"));
+	
+	//FString은 *을 붙혀줘야 TCHAR배열에 반환이 된다
+	UE_LOG(LogTemp, Log, TEXT("Strings1: %s"), *String1);
 
-	//절대경로
-	FPaths::MakeStandardFilename(AbsolutePathForRawData);
-	UE_LOG(LogTemp, Log, TEXT("Absolute path for saved file: %s"), *AbsolutePathForRawData);
-
-	FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*AbsolutePathForRawData);
-	if (nullptr != RawFileWriterAr)
+	const TCHAR* PtrToTChar = *String1;
+	TCHAR* RawPtrToTChar = String1.GetCharArray().GetData();
+	//ESearchCase::IgnoreCase : 대소문자를 무시한다
+	if (String1.Contains(TEXT("unreal"), ESearchCase::IgnoreCase) == true)
 	{
-		*RawFileWriterAr << SrcRawData;
-		RawFileWriterAr->Close();
-		delete RawFileWriterAr;
-		RawFileWriterAr = nullptr;
+		int32 Index = String1.Find(TEXT("unreal"), ESearchCase::IgnoreCase);
+		FString FoundedString = String1.Mid(Index);
+		// "unreal" 문자열이 시작되는 곳에서부터 마지막까지 자름.
+		UE_LOG(LogTemp, Log, TEXT("Founded string: %s"), *FoundedString);
 	}
 
-	FBirdData DstRawData;
-	FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*AbsolutePathForRawData);
-	if (nullptr != RawFileReaderAr)
-	{
-		*RawFileReaderAr << DstRawData;
-		RawFileReaderAr->Close();
-		delete RawFileReaderAr;
-		RawFileReaderAr = nullptr;
+	int32 IntValue = 7;
+	float FloatValue = 3.1415f;
 
-		UE_LOG(LogTemp, Log, TEXT("[DstRawData] Name: %s, ID: %d"), *DstRawData.Name, DstRawData.ID);
-	}
+	//문자열과 숫자를 합치고 싶을 때 사용
+	FString StringWithNumbers = FString::Printf(TEXT("int32 : %d and float : %f"), IntValue, FloatValue);
+	UE_LOG(LogTemp, Log, TEXT("String With Numbers : %s"), *StringWithNumbers);
 
-	SerializedPigeon = NewObject<USpigeon>();
-	SerializedPigeon->SetName(TEXT("Pigeon76"));
-	SerializedPigeon->SetID(76);
-	UE_LOG(LogTemp, Log, TEXT("[SerializedPigeon] Name: %s, ID: %d"), *SerializedPigeon->GetName(), SerializedPigeon->GetID());
+	//Int형을 FString으로 변경해줌
+	FString IntString = FString::FromInt(IntValue);
+	UE_LOG(LogTemp, Log, TEXT("IntString: %s"), *IntString);
 
-	const FString ObjectDataFileName(TEXT("ObjectData.bin"));
-	FString AbsolutePathForObjectData = FPaths::Combine(*SavedDir, *ObjectDataFileName);
-	FPaths::MakeStandardFilename(AbsolutePathForObjectData);
+	// float 자료형의 표준은 상당히 복잡함. 이를 정돈해서 문자열로 바꿔줌.
+	FString FloatString = FString::SanitizeFloat(FloatValue);
+	UE_LOG(LogTemp, Log, TEXT("FloatString: %s"), *FloatString);
 
-	TArray<uint8> BufferArray;
-	FMemoryWriter MemoryWriterAr(BufferArray);
-	SerializedPigeon->Serialize(MemoryWriterAr);
+	//Atoi는 메모리 버퍼 공격에 취약하기때문에 권장되지 않는 함수이다
+	int32 IntValueFromString = FCString::Atoi(*IntString);
+	// C 문자열 라이브러리 함수(atoi(), strcpy(), ...)를 제공하는 클래스 FCString.
+	UE_LOG(LogTemp, Log, TEXT("IntValueFromString: %d"), IntValueFromString);
 
-	TUniquePtr<FArchive> ObjectDataFileWriterAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileWriter(*AbsolutePathForObjectData));
-	if (nullptr != ObjectDataFileWriterAr)
-	{
-		*ObjectDataFileWriterAr << BufferArray;
-		ObjectDataFileWriterAr->Close();
-
-		ObjectDataFileWriterAr = nullptr; //delete ObjectDataFileWriterAr; 와 같은 효과.
-	}
-
-	TArray<uint8> BufferArrayFromObjectDataFile;
-	TUniquePtr<FArchive> ObjectDataFileReaderAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileReader(*AbsolutePathForObjectData));
-	if (nullptr != ObjectDataFileReaderAr)
-	{
-		*ObjectDataFileReaderAr << BufferArrayFromObjectDataFile;
-		ObjectDataFileReaderAr->Close();
-
-		ObjectDataFileReaderAr = nullptr;
-	}
-
-	FMemoryReader MemoryReaderAr(BufferArrayFromObjectDataFile);
-	USpigeon* Pigeon77 = NewObject<USpigeon>();
-	Pigeon77->Serialize(MemoryReaderAr);
-	UE_LOG(LogTemp, Log, TEXT("[Pigeon77] Name: %s, ID: %d"), *Pigeon77->GetName(), Pigeon77->GetID());
-
-	const FString JsonDataFileName(TEXT("StudyJsonFile.txt"));
-	FString AbsolutePathForJsonData = FPaths::Combine(*SavedDir, *JsonDataFileName);
-	FPaths::MakeStandardFilename(AbsolutePathForJsonData);
-
-	TSharedRef<FJsonObject> SrcJsonObject = MakeShared<FJsonObject>();
-	FJsonObjectConverter::UStructToJsonObject(SerializedPigeon->GetClass(), SerializedPigeon, SrcJsonObject);
-
-	FString JsonOutString;
-	TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);
-	if (true == FJsonSerializer::Serialize(SrcJsonObject, JsonWriterAr))
-	{
-		FFileHelper::SaveStringToFile(JsonOutString, *AbsolutePathForJsonData);
-	}
-
-	FString JsonInString;
-	FFileHelper::LoadFileToString(JsonInString, *AbsolutePathForJsonData);
-	TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
-
-	TSharedPtr<FJsonObject> DstJsonObject;
-	if (true == FJsonSerializer::Deserialize(JsonReaderAr, DstJsonObject))
-	{
-		USpigeon* Pigeon78 = NewObject<USpigeon>();
-		if (true == FJsonObjectConverter::JsonObjectToUStruct(DstJsonObject.ToSharedRef(), Pigeon78->GetClass(), Pigeon78))
-		{
-			UE_LOG(LogTemp, Log, TEXT("[Pigeon78] Name: %s, ID: %d"), *Pigeon78->GetName(), Pigeon78->GetID());
-		}
-	}
 }
 
 void USGameInstance::Shutdown()
